0b872ad20a019d73391d792c8bba44a1674eb9ed
林拓海 <hayashitakumi@hayashitakuminoMacBook-Pro-2.local>
2026-03-31 18:06:12 +0900

    fix: GA page_view

--- app/GA.tsx
+++ app/GA.tsx
@@ -1,19 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
 declare global {
   interface Window {
     gtag: (...args: any[]) => void;
   }
 }
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 
 export default function GA() {
   const pathname = usePathname();
 
   useEffect(() => {
     if (typeof window.gtag !== "undefined") {
-      window.gtag("config", "G-F52LGM1JDL", {
+      window.gtag("event", "page_view", {
         page_path: pathname,
       });
     }
