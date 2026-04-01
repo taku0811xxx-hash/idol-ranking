39e67fb727461a5553d20158d3a748448615ba0e
林拓海 <hayashitakumi@hayashitakuminoMacBook-Pro-2.local>
2026-04-01 17:17:42 +0900

    deploy: レイアウト-1

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { db } from "@/firebase";
